434a7562cfe83398d905d46c1f8d46ed3231e47a
unknown <khalitiman17@gmail.com>
2024-12-17 12:49:05 +0300

    added more routes and fixed the jwt middleware

--- server/api.go
+++ server/api.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/joho/godotenv"
+	"github.com/rs/cors"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -62,11 +63,26 @@ type Claims struct {
 	jwt.StandardClaims
 }
 
+type Fines struct {
+	ID       int     `json:"id"`
+	UserID   int     `json:"user_id"`
+	BookID   int     `json:"book_id"`
+	FineAmt  float64 `json:"fine_amount"`
+	FineDate string  `json:"fine_date"`
+}
 
+type Borrowings struct {
+	ID         int    `json:"id"`
+	UserId     int    `json:"user_id"`
+	BookId     int    `json:"book_id"`
+	BorrowDate string `json:"borrow_date"`
+	ReturnDate *string `json:"return_date"`
+	DueDate    string `json:"due_date"`
+	Renewed    bool   `json:"renewed"`
+}
 
 var secretKey []byte
 var expirationTime time.Time = time.Now().Add(24 * time.Hour)
-
 
 func NewApiserver(addr string) *ApiServer {
 	return &ApiServer{
@@ -90,10 +106,20 @@ func (s *ApiServer) Run() error {
 	mux.HandleFunc("POST /signup", signUp)
 	mux.HandleFunc("POST /login", login)
 	mux.HandleFunc("GET /users", getUsers)
+	mux.HandleFunc("GET /fines", getFines)
+	mux.HandleFunc("GET /borrowings", borrowings)
 	// mux.HandleFunc("PUT /update", updateUserDetails)
+	corsMiddleware := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"}, // Replace with your frontend URL
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+	})
 
+	// Wrap mux with CORS middleware
+	handler := corsMiddleware.Handler(mux)
 	srv := &http.Server{
-		Handler: mux,
+		Handler: handler,
 		Addr:    s.addr,
 	}
 
@@ -121,7 +147,7 @@ func getBooks(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusFound)
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(books)
 }
 
@@ -272,10 +298,10 @@ func login(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Failed to generate Token,", http.StatusInternalServerError)
 		return
 	}
-	cookie := http.Cookie {
-		Name: "token",
-		Value: tokenStr,
-		Expires: expirationTime,
+	cookie := http.Cookie{
+		Name:     "token",
+		Value:    tokenStr,
+		Expires:  expirationTime,
 		HttpOnly: true,
 	}
 	http.SetCookie(w, &cookie)
@@ -285,7 +311,7 @@ func login(w http.ResponseWriter, r *http.Request) {
 }
 
 func getUsers(w http.ResponseWriter, r *http.Request) {
-	var users[] User
+	var users []User
 	query := `SELECT id, email, username, role from users`
 	rows, err := DB.Query(query)
 	if err != nil {
@@ -311,7 +337,7 @@ func getUsers(w http.ResponseWriter, r *http.Request) {
 }
 
 func createToken(userID int, username, role string) (string, error) {
-	
+
 	claims := &Claims{
 		UserID:   userID,
 		Username: username,
@@ -339,12 +365,11 @@ func extractUserIDFromCookie(r *http.Request) (int, error) {
 	})
 
 	if err != nil || !token.Valid {
-        return 0, fmt.Errorf("invalid token")
-    }
+		return 0, fmt.Errorf("invalid token")
+	}
 
-    return claims.UserID, nil
+	return claims.UserID, nil
 }
-
 
 // func updateUserDetails(w http.ResponseWriter, r *http.Request){
 
@@ -352,63 +377,60 @@ func extractUserIDFromCookie(r *http.Request) (int, error) {
 
 // // Borrowing and Returning routes
 func borrow(w http.ResponseWriter, r *http.Request) {
-    // Extract user ID from cookie
-    userID, err := extractUserIDFromCookie(r)
-    if err != nil {
-        http.Error(w, "Unauthorized: "+err.Error(), http.StatusUnauthorized)
-        return
-    }
+	// Extract user ID from cookie
+	userID, err := extractUserIDFromCookie(r)
+	if err != nil {
+		http.Error(w, "Unauthorized: "+err.Error(), http.StatusUnauthorized)
+		return
+	}
 
-    // Parse book ID from request path
-    idStr := r.PathValue("id") // Assuming r.PathValue is a custom method to extract path variables
-    bookId, err := strconv.Atoi(idStr)
-    if err != nil {
-        http.Error(w, "Invalid book ID", http.StatusBadRequest)
-        return
-    }
+	// Parse book ID from request path
+	idStr := r.PathValue("id") // Assuming r.PathValue is a custom method to extract path variables
+	bookId, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid book ID", http.StatusBadRequest)
+		return
+	}
 
-    // Check if the book is available
-    var availability bool
-    query := `SELECT availability FROM books WHERE id = $1`
-    err = DB.QueryRow(query, bookId).Scan(&availability)
-    if err != nil {
-        log.Printf("Error checking book availability: %v", err)
-        http.Error(w, "Failed to check book availability", http.StatusInternalServerError)
-        return
-    }
-    if !availability {
-        http.Error(w, "Book is already borrowed", http.StatusBadRequest)
-        return
-    }
+	// Check if the book is available
+	var availability bool
+	query := `SELECT availability FROM books WHERE id = $1`
+	err = DB.QueryRow(query, bookId).Scan(&availability)
+	if err != nil {
+		log.Printf("Error checking book availability: %v", err)
+		http.Error(w, "Failed to check book availability", http.StatusInternalServerError)
+		return
+	}
+	if !availability {
+		http.Error(w, "Book is already borrowed", http.StatusBadRequest)
+		return
+	}
 
-    // Update book availability
-    query = `UPDATE books SET availability = false WHERE id = $1`
-    _, err = DB.Exec(query, bookId)
-    if err != nil {
-        log.Printf("Error updating book availability: %v", err)
-        http.Error(w, "Failed to borrow book", http.StatusInternalServerError)
-        return
-    }
+	// Update book availability
+	query = `UPDATE books SET availability = false WHERE id = $1`
+	_, err = DB.Exec(query, bookId)
+	if err != nil {
+		log.Printf("Error updating book availability: %v", err)
+		http.Error(w, "Failed to borrow book", http.StatusInternalServerError)
+		return
+	}
 
-    // Insert borrowing record
-    borrowDate := time.Now()
-    dueDate := borrowDate.AddDate(0, 0, 14) // Default borrow duration is 14 days
-    renewed := false
-    query = `INSERT INTO borrowings (user_id, book_id, borrow_date, due_date, renewed) VALUES ($1, $2, $3, $4, $5)`
-    _, err = DB.Exec(query, userID, bookId, borrowDate, dueDate, renewed)
-    if err != nil {
-        log.Printf("Error inserting borrowing record: %v", err)
-        http.Error(w, "Failed to update borrowings", http.StatusInternalServerError)
-        return
-    }
+	// Insert borrowing record
+	borrowDate := time.Now()
+	dueDate := borrowDate.AddDate(0, 0, 14) // Default borrow duration is 14 days
+	renewed := false
+	query = `INSERT INTO borrowings (user_id, book_id, borrow_date, due_date, renewed) VALUES ($1, $2, $3, $4, $5)`
+	_, err = DB.Exec(query, userID, bookId, borrowDate, dueDate, renewed)
+	if err != nil {
+		log.Printf("Error inserting borrowing record: %v", err)
+		http.Error(w, "Failed to update borrowings", http.StatusInternalServerError)
+		return
+	}
 
-    // Respond with success
-    w.WriteHeader(http.StatusOK)
-    w.Write([]byte("Successfully borrowed book"))
+	// Respond with success
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Successfully borrowed book"))
 }
-
-
-
 
 func returning(w http.ResponseWriter, r *http.Request) {
 	idStr := r.PathValue("id")
@@ -417,7 +439,7 @@ func returning(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	
+
 	var availability bool
 	query := `SELECT availability FROM books WHERE id = $1`
 	err = DB.QueryRow(query, bookId).Scan(&availability)
@@ -431,32 +453,31 @@ func returning(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Retrieve the due date and user ID for the book borrowing
-    var userID int
-    var dueDate time.Time
-    query = `SELECT user_id, due_date FROM borrowings WHERE book_id = $1 AND return_date IS NULL`
-    err = DB.QueryRow(query, bookId).Scan(&userID, &dueDate)
-    if err != nil {
-        http.Error(w, "Failed to retrieve borrowing details", http.StatusInternalServerError)
-        return
-    }
+	var userID int
+	var dueDate time.Time
+	query = `SELECT user_id, due_date FROM borrowings WHERE book_id = $1 AND return_date IS NULL`
+	err = DB.QueryRow(query, bookId).Scan(&userID, &dueDate)
+	if err != nil {
+		http.Error(w, "Failed to retrieve borrowing details", http.StatusInternalServerError)
+		return
+	}
 
-    // Check if the book is overdue
-    returnDate := time.Now()
-    if returnDate.After(dueDate) {
-        // Calculate the number of days overdue
-        daysOverdue := int(returnDate.Sub(dueDate).Hours() / 24)
-        fineAmount := float64(daysOverdue * 5) // Example: $5 per day overdue
+	// Check if the book is overdue
+	returnDate := time.Now()
+	if returnDate.After(dueDate) {
+		// Calculate the number of days overdue
+		daysOverdue := int(returnDate.Sub(dueDate).Hours() / 24)
+		fineAmount := float64(daysOverdue * 5) // Example: $5 per day overdue
 
-        // Insert fine record into the fines table
-        query = `INSERT INTO fines (user_id, book_id, fine_amount, fine_date) VALUES ($1, $2, $3, $4)`
-        _, err = DB.Exec(query, userID, bookId, fineAmount, returnDate.Format("2006-01-02"))
-        if err != nil {
-            log.Printf("Database error: %v", err)
-            http.Error(w, "Failed to create fine", http.StatusInternalServerError)
-            return
-        }
-    }
-	
+		// Insert fine record into the fines table
+		query = `INSERT INTO fines (user_id, book_id, fine_amount, fine_date) VALUES ($1, $2, $3, $4)`
+		_, err = DB.Exec(query, userID, bookId, fineAmount, returnDate.Format("2006-01-02"))
+		if err != nil {
+			log.Printf("Database error: %v", err)
+			http.Error(w, "Failed to create fine", http.StatusInternalServerError)
+			return
+		}
+	}
 
 	// Update availability to true
 	query = `UPDATE books SET availability = true WHERE id = $1`
@@ -466,16 +487,72 @@ func returning(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	
-
 	query = `UPDATE borrowings SET return_date = $1 WHERE book_id = $2`
 	_, err = DB.Exec(query, returnDate, bookId)
 	if err != nil {
-	log.Printf("failed to update return date %v", err)
-	http.Error(w, "Failed to update the return Date", http.StatusInternalServerError)
-	return
-}
+		log.Printf("failed to update return date %v", err)
+		http.Error(w, "Failed to update the return Date", http.StatusInternalServerError)
+		return
+	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Successfully returned book"))
+}
+
+func getFines(w http.ResponseWriter, r *http.Request) {
+	var fines []Fines
+
+	query := `SELECT * FROM fines`
+	rows, err := DB.Query(query)
+	if err != nil {
+		http.Error(w, "Unable to Query Fines Table", http.StatusInternalServerError)
+		return
+	}
+
+	for rows.Next() {
+		var fine Fines
+		if err := rows.Scan(&fine.ID, &fine.UserID, &fine.BookID, &fine.FineAmt, &fine.FineDate); err != nil {
+			http.Error(w, "Unable to Scan Fines", http.StatusInternalServerError)
+			return
+		}
+		fines = append(fines, fine)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(fines)
+}
+
+func borrowings(w http.ResponseWriter, r *http.Request) {
+	var borrowings []Borrowings
+	// Query to fetch data from the borrowings table
+	query := `SELECT id, user_id, book_id, borrow_date, return_date, due_date, renewed FROM borrowings`
+	rows, err := DB.Query(query)
+	if err != nil {
+		log.Printf("Error querying borrowings: %v", err)
+		http.Error(w, "Unable to query borrowings", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close() // Ensure rows are closed after use
+
+	// Iterate through query results
+	for rows.Next() {
+		var borrow Borrowings
+		if err := rows.Scan(&borrow.ID, &borrow.UserId, &borrow.BookId, &borrow.BorrowDate, &borrow.ReturnDate, &borrow.DueDate, &borrow.Renewed); err != nil {
+			log.Printf("Error scanning borrowings: %v", err)
+			http.Error(w, "Unable to scan borrowings", http.StatusInternalServerError)
+			return
+		}
+		borrowings = append(borrowings, borrow)
+	}
+
+	// Check if no borrowings were found
+	if len(borrowings) == 0 {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "No borrowings found"}`))
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(borrowings)
 }
--- server/middleware.go
+++ server/middleware.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/dgrijalva/jwt-go"
 )
@@ -10,9 +12,10 @@ import (
 
 func adminMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		tokenString := r.Header.Get("Authoriazation")
+		tokenString := r.Header.Get("Authorization")
 		if tokenString == "" {
 			http.Error(w, "Missing Token", http.StatusUnauthorized)
+			return
 		}
 		claims := &Claims{}
 		token, err := jwt.ParseWithClaims(tokenString, claims, func(t *jwt.Token) (interface{}, error) {
@@ -23,7 +26,7 @@ func adminMiddleware(next http.HandlerFunc) http.HandlerFunc {
 			return
 		}
 		if claims.Role != "admin"{
-			http.Error(w, "Unautharized access", http.StatusUnauthorized)
+			http.Error(w, "Unauthorized access", http.StatusUnauthorized)
 			return
 		}
 
@@ -32,20 +35,40 @@ func adminMiddleware(next http.HandlerFunc) http.HandlerFunc {
 }
 
 func jwtMiddleware(next http.Handler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		tokenString := r.Header.Get("Authorization")
-		if tokenString == "" {
-			http.Error(w, "Missing Token", http.StatusUnauthorized)
-			return
-		}
-		claims := &Claims{}
-		token, err := jwt.ParseWithClaims(tokenString, claims, func(t *jwt.Token) (interface{}, error) {
-			return secretKey, nil
-		})
-		if err != nil || !token.Valid {
-			http.Error(w, "Invalid Token", http.StatusUnauthorized)
-			return
-		}
-		next.ServeHTTP(w, r)
-	}
+    return func(w http.ResponseWriter, r *http.Request) {
+        // Retrieve the Authorization header
+        tokenString := r.Header.Get("Authorization")
+        if tokenString == "" {
+            http.Error(w, "Missing Token", http.StatusUnauthorized)
+            return
+        }
+
+        // Ensure the token includes "Bearer " prefix
+        if !strings.HasPrefix(tokenString, "Bearer ") {
+            http.Error(w, "Invalid Token Format", http.StatusUnauthorized)
+            return
+        }
+
+        // Remove the "Bearer " prefix to get the token
+        tokenString = strings.TrimPrefix(tokenString, "Bearer ")
+
+        // Parse the token and validate claims
+        claims := &Claims{}
+        token, err := jwt.ParseWithClaims(tokenString, claims, func(t *jwt.Token) (interface{}, error) {
+            // Ensure the signing method is correct
+            if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+                return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+            }
+            return secretKey, nil
+        })
+
+        // Handle token parsing errors or invalid tokens
+        if err != nil || !token.Valid {
+            http.Error(w, "Invalid Token", http.StatusUnauthorized)
+            return
+        }
+
+        // Token is valid, pass control to the next handler
+        next.ServeHTTP(w, r)
+    }
 }
